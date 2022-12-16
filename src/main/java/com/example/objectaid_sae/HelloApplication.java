7f8402af8773a57a93be4248cdd5c59bcf3bbfd0
miesch3u <nathanaelmiesch@gmail.com>
2022-12-16 18:00:10 +0100

    dossier Document

--- src/main/java/com/example/objectaid_sae/HelloApplication.java
+++ src/main/java/com/example/objectaid_sae/HelloApplication.java
@@ -1,16 +1,18 @@
 package com.example.objectaid_sae;
 
 import com.example.objectaid_sae.controleur.ControleurClasseCliquer;
+import com.example.objectaid_sae.controleur.ControleurFichierGlisse;
 import com.example.objectaid_sae.model.Classe;
 import com.example.objectaid_sae.model.Model;
 import com.example.objectaid_sae.vue.VueClasse;
-import com.example.objectaid_sae.vue.VueMenuTemporaire;
+import com.example.objectaid_sae.vue.VueFichiers;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -20,21 +22,19 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Pane pane = new Pane();
-
-        Classe classe = new Classe();
-        VueMenuTemporaire vueTemp = new VueMenuTemporaire(classe);
-        vueTemp.setCacher(false);
-        classe.ajouterObservateur(vueTemp);
-        VueClasse vue = new VueClasse(classe, vueTemp);
-        classe.ajouterObservateur(vue);
+        BorderPane bp = new BorderPane();
+        /*Classe classe = new Classe();
         classe.addAttribut(1, "- premier: String");
         classe.addAttribut(1, "- deuxieme: int");
         classe.addMethode(1, "+ methode1 () : String");
         classe.addMethode(2, "+ methode2 () : int");
+        VueClasse vue = new VueClasse(classe);
         vue.notifier(classe);
-
-        pane.getChildren().addAll(vue, vueTemp);
-        Scene scene = new Scene(pane, 320, 240);
+        ControleurClasseCliquer controleurClasseCliquer = new ControleurClasseCliquer(classe);
+        pane.getChildren().add(vue);*/
+        bp.setCenter(pane);
+        bp.setLeft(new VueFichiers("C:/xampp/htdocs/WEB/ObjectAid_SAE/src/main/java"));
+        Scene scene = new Scene(bp, 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
