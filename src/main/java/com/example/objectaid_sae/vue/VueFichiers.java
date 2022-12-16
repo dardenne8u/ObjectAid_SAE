20dfdb25c41ebffa3a905852ab9562835d8b739d
Nathan <Nathan.Melbeck@gmail.com>
2022-12-16 15:06:53 +0100

    Vufichier pane

--- src/main/java/com/example/objectaid_sae/vue/VueFichiers.java
+++ src/main/java/com/example/objectaid_sae/vue/VueFichiers.java
@@ -3,62 +3,60 @@ package com.example.objectaid_sae.vue;
 import com.example.objectaid_sae.observateur.Observateur;
 import com.example.objectaid_sae.observateur.Sujet;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.io.File;
 
-public class VueFichiers extends BorderPane implements Observateur {
+public class VueFichiers extends Pane implements Observateur {
 
 
-
-    public VueFichiers(String path){
+    public VueFichiers(String path) {
         File f = new File(path);
         TreeView<HBox> arbre = new TreeView<>(tree(f));
-        this.setLeft(arbre);
-
-
+         this.getChildren().add(arbre);
 
 
     }
 
     public TreeItem<HBox> tree(File file) {
         TreeItem<HBox> res = null;
-            if (file.exists()) {
-                res = creerItem(file.getName());
-                    if (file.isDirectory()) {
-                        File[] files = file.listFiles();
-                        for (int i = 0; i < files.length; i++) {
-                            if (files[i].isDirectory()) {
-                                res.getChildren().add(tree(files[i]));
-                            } else {
-                                res.getChildren().add(creerItem(files[i].getName()));
-                            }
-                        }
+        if (file.exists()) {
+            res = creerItem(file.getName());
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isDirectory()) {
+                        res.getChildren().add(tree(files[i]));
+                    } else {
+                        res.getChildren().add(creerItem(files[i].getName()));
                     }
-
                 }
-        return res;
             }
 
+        }
+        return res;
+    }
 
-public TreeItem<HBox> creerItem(String text){
+
+    public TreeItem<HBox> creerItem(String text) {
         HBox box = new HBox();
-    CheckBox check = new CheckBox();
-    check.setDisable(true);
-    Text t = new Text(text);
+        box.setSpacing(5);
+        CheckBox check = new CheckBox();
+        check.setDisable(true);
+        Text t = new Text(text);
 
-    box.getChildren().addAll(check, t);
+        box.getChildren().addAll(check, t);
 
-       return new TreeItem<HBox>(box);
-}
-
-
-
-
+        TreeItem<HBox> res = new TreeItem<HBox>(box);
+        res.setExpanded(true);
+        return res;
+    }
 
 
     @Override
