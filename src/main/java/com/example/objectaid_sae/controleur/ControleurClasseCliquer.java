f56d442751d2170425e10231d7b9ea8b53b66720
miesch3u <nathanaelmiesch@gmail.com>
2022-12-16 10:44:16 +0100

    ajouter des attributs et methodes Handle des controleurs

--- src/main/java/com/example/objectaid_sae/controleur/ControleurClasseCliquer.java
+++ src/main/java/com/example/objectaid_sae/controleur/ControleurClasseCliquer.java
@@ -1,4 +1,14 @@
 package com.example.objectaid_sae.controleur;
 
-public class ControleurClasseCliquer {
+import com.example.objectaid_sae.model.Model;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class ControleurClasseCliquer implements EventHandler<MouseEvent> {
+
+    Model mod;
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+
+    }
 }
---
+++ src/main/java/com/example/objectaid_sae/controleur/ControleurFichierGlisse.java
@@ -0,0 +1,13 @@
+package com.example.objectaid_sae.controleur;
+
+import com.example.objectaid_sae.model.Model;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseDragEvent;
+
+public class ControleurFichierGlisse implements EventHandler<MouseDragEvent> {
+
+    Model mod;
+    @Override
+    public void handle(MouseDragEvent mouseDragEvent) {
+    }
+}
