459220b375e416ea6f15b808fff6896d45ef11c4
drakoghir <gdardenne0@gmail.com>
2022-12-16 10:36:07 +0100

    Implementation Sujet au Model.java

--- src/main/java/com/example/objectaid_sae/model/Model.java
+++ src/main/java/com/example/objectaid_sae/model/Model.java
@@ -1,13 +1,18 @@
 package com.example.objectaid_sae.model;
 
+import com.example.objectaid_sae.observateur.Observateur;
+import com.example.objectaid_sae.observateur.Sujet;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Model {
+public class Model implements Sujet {
     private List<Classe> classes;
+    private List<Observateur> observateurs;
 
     public Model() {
         this.classes = new ArrayList<>();
+        this.observateurs = new ArrayList<>();
     }
 
     public void addClasse(Classe classe) {
@@ -17,5 +22,18 @@ public class Model {
 
     public List<Classe> getClasses() {
         return this.classes;
+    }
+
+    @Override
+    public void ajouterObservateur(Observateur o) {
+        if(!this.observateurs.contains(o))
+            this.observateurs.add(o);
+    }
+
+    @Override
+    public void notifierObservateurs() {
+        for(Observateur o : this.observateurs) {
+            o.notifier(this);
+        }
     }
 }
--- src/main/java/com/example/objectaid_sae/modele/Classe.java
+++
@@ -1,4 +0,0 @@
-package com.example.objectaid_sae.modele;
-
-public class Classe {
-}
