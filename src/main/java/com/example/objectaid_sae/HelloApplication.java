5df0c1d35015a8c3f3372b57a77d312eb050b44d
miesch3u <nathanaelmiesch@gmail.com>
2022-12-16 10:57:03 +0100

    tests TreeView

--- src/main/java/com/example/objectaid_sae/HelloApplication.java
+++ src/main/java/com/example/objectaid_sae/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.objectaid_sae;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        TreeItem<String> treeee = new TreeItem<>("coucou");
+        treeee.getChildren().add(new TreeItem<>("coucou2"));
+        TreeView<String> tree = new TreeView<>(treeee);
+        Scene scene = new Scene(tree);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
