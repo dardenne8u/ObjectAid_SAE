d0ffdf7e317c1bfba3d14de8b0e8b6fd5dbe73d1
Lucie <lucye.laury@gmail.com>
2023-01-05 18:06:22 +0100

    début réflexion sur l'inclinaison de la flèche pour suivre la droite

--- src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueLignePleine.java
+++ src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueLignePleine.java
@@ -4,10 +4,21 @@ import javafx.scene.shape.Line;
 
 public class FabriqueLignePleine implements FabriqueLigne{
 
+    private double xc1;
+    private double yc1;
+    private double xc2;
+    private double yc2;
+
+    public FabriqueLignePleine(double xc1, double yc1, double xc2, double yc2){
+        this.xc1 = xc1;
+        this.yc1 = yc1;
+        this.xc2 = xc2;
+        this.yc2 = yc2;
+    }
 
 
     @Override
     public Line fabriquer() {
-        return new Line();
+        return new Line(xc1, yc1, xc2, yc2);
     }
 }
--- src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriquePolyBlanc.java
+++ src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriquePolyBlanc.java
@@ -4,13 +4,27 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Polygon;
 
 public class FabriquePolyBlanc implements FabriquePolygone{
+
+    private double xc1;
+    private double yc1;
+    private double xc2;
+    private double yc2;
+
+    public FabriquePolyBlanc(double xc1, double yc1, double xc2, double yc2){
+        this.xc1 = xc1;
+        this.yc1 = yc1;
+        this.xc2 = xc2;
+        this.yc2 = yc2;
+    }
+
+
     @Override
     public Polygon fabriquer() {
         Polygon poly = new Polygon();
         poly.getPoints().addAll(new Double[]{
-                0.0, 0.0,
-                0.0, 20.0,
-                20.0, 0.0,
+                xc1, yc1,
+                xc1, (yc1+20.0),
+                (xc1+20.0), yc1,
 
         });
         poly.setFill(Paint.valueOf("white"));
