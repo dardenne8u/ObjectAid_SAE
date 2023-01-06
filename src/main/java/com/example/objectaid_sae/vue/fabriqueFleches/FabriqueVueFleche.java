5d2aa0493b8610178cef856b0f3fdb93ab97c339
Nathan <Nathan.Melbeck@gmail.com>
2023-01-06 16:36:58 +0100

    Bug incomprehensible

---
+++ src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueVueFleche.java
@@ -0,0 +1,7 @@
+package com.example.objectaid_sae.vue.fabriqueFleches;
+
+import com.example.objectaid_sae.vue.VueFleche;
+
+public interface FabriqueVueFleche {
+    VueFleche fabriquer();
+}
---
+++ src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueVueFlecheImplement.java
@@ -0,0 +1,45 @@
+package com.example.objectaid_sae.vue.fabriqueFleches;
+
+import com.example.objectaid_sae.model.Fleche;
+import com.example.objectaid_sae.vue.VueFleche;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Rotate;
+
+public class FabriqueVueFlecheImplement implements FabriqueVueFleche {
+
+    Fleche fleche;
+    FabriquePolyBlanc fbpoly;
+
+
+    public FabriqueVueFlecheImplement(Fleche f){
+        fleche = f;
+        fbpoly = new FabriquePolyBlanc(-10,-10,-10,-10);
+    }
+    @Override
+    public VueFleche fabriquer() {
+        return faireFleche(fleche.getDepart().getX(), fleche.getDepart().getY(), fleche.getArrivee().getX(), fleche.getArrivee().getY());
+    }
+
+    private VueFleche faireFleche(double x1, double y1, double x2, double y2){
+        double len = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
+        double inclinaison = Math.atan2(y2 - y1, x2 - x1)* 180/Math.PI+90;
+        VueFleche res = new VueFleche();
+        Polygon poly;
+        poly = fbpoly.fabriquer();
+        res.getChildren().add(poly);
+        FabriqueLignePoint fbLigne = new FabriqueLignePoint(0,0,0,len);
+        Line l = fbLigne.fabriquer();
+        res.getChildren().add(l);
+        Rotate r = new Rotate();
+        r.setPivotX(0);
+        r.setPivotY(0);
+        r.setAngle(inclinaison);
+        (res.getChildren().get(0)).setRotate(45);
+        (res.getChildren().get(0)).setLayoutY(10);
+        res.setLayoutY(y2);
+        res.setLayoutX(y1);
+        res.getTransforms().add(r);
+        return res;
+    }
+}
