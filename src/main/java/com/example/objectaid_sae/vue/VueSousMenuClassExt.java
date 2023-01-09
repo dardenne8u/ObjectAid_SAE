70b1db25f4c3a8f7f3a5fb68df80b7b520c038a5
miesch3u <nathanaelmiesch@gmail.com>
2023-01-09 16:44:24 +0100

    fleches qui suivent les classent (a peu pres)

--- src/main/java/com/example/objectaid_sae/vue/VueSousMenuClassExt.java
+++ src/main/java/com/example/objectaid_sae/vue/VueSousMenuClassExt.java
@@ -2,6 +2,7 @@ package com.example.objectaid_sae.vue;
 
 import com.example.objectaid_sae.model.Analyseur;
 import com.example.objectaid_sae.model.Classe;
+import com.example.objectaid_sae.model.Fleche;
 import com.example.objectaid_sae.model.Model;
 import com.example.objectaid_sae.observateur.Observateur;
 import com.example.objectaid_sae.observateur.Sujet;
@@ -33,6 +34,7 @@ public class VueSousMenuClassExt extends VBox implements Observateur {
                         c = new Analyseur(packageExt).analyseClasse();
                         VueClasse vue = new VueClasse(c);
                         centre.getChildren().add(vue);
+                        Fleche.creerFleches(c, (VueCentre) centre);
                         Model.getModel().addClasse(c);
                         lab.setDisable(true);
                     } catch (ClassNotFoundException e) {
--- src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueVueFleche.java
+++ src/main/java/com/example/objectaid_sae/vue/fabriqueFleches/FabriqueVueFleche.java
@@ -1,7 +1,31 @@
 package com.example.objectaid_sae.vue.fabriqueFleches;
 
 import com.example.objectaid_sae.vue.VueFleche;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Rotate;
 
-public interface FabriqueVueFleche {
-    VueFleche fabriquer();
+public abstract class FabriqueVueFleche {
+    public abstract VueFleche fabriquer();
+
+    protected VueFleche build(FabriqueLigne fbLigne, FabriquePolygone fbpoly, double inclinaison, double offset, double x, double y){
+        VueFleche res = new VueFleche();
+        Polygon poly;
+        poly = fbpoly.fabriquer();
+        Line l = fbLigne.fabriquer();
+        Rotate r = new Rotate();
+        r.setPivotX(0);
+        r.setPivotY(0);
+        r.setAngle(inclinaison);
+        l.setLayoutY(offset);
+        poly.setRotate(45);
+        poly.setLayoutX(-10);
+        poly.setLayoutY(offset);
+        res.getChildren().add(poly);
+        res.getChildren().add(l);
+        res.setLayoutY(y);
+        res.setLayoutX(x);
+        res.getTransforms().add(r);
+        return res;
+    }
 }
